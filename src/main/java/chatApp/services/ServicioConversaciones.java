654f032efe830eab4457e975dcb0cba6d17c6a5e
Roberto <gitrob@hotmail.com>
2024-09-23 14:02:00 +0200

    vxx

--- src/main/java/chatApp/services/ServicioConversaciones.java
+++ src/main/java/chatApp/services/ServicioConversaciones.java
@@ -1,5 +1,47 @@
 package chatApp.services;
 
+import java.time.LocalDate;
+
+import chatApp.exceptions.ConversacionException;
+import chatApp.model.Conversacion;
+import chatApp.model.Conversacion.TipoAgente;
+
 public class ServicioConversaciones implements IServicioConversaciones {
+
+	@Override
+	public void registraNuevaConveracion(TipoAgente tipo, String pregunta, String respuesta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Conversacion getRecuperaConversacion(TipoAgente tipo, String pregunta, LocalDate fecha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eliminaConversacion(LocalDate fecha, TipoAgente tipo) throws ConversacionException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean incrementaNumeroValoraciones(LocalDate fecha, TipoAgente tipo, String pregunta) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getValoracionMediaParaHumanos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getValoracionMedidaParaBots() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
